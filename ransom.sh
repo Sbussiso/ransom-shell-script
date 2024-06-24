590149fcd7baaa05fdbd155a5c4604abe55117b9
S'Bussiso Dube <80188685+Sbussiso@users.noreply.github.com>
2024-06-23 21:39:16 -0700

    added more package managers

--- ransom.sh
+++ ransom.sh
@@ -17,16 +17,19 @@ if command -v apt > /dev/null; then
     apt install -y git python3 python3-venv python3-pip
 elif command -v yum > /dev/null; then
     yum update -y
-    yum install -y git python3 python3-pip
-    python3 -m ensurepip
-    python3 -m pip install --upgrade pip
-    python3 -m pip install virtualenv
+    yum install -y git python3 python3-venv python3-pip
 elif command -v dnf > /dev/null; then
     dnf update -y
-    dnf install -y git python3 python3-pip
-    python3 -m ensurepip
-    python3 -m pip install --upgrade pip
-    python3 -m pip install virtualenv
+    dnf install -y git python3 python3-venv python3-pip
+elif command -v zypper > /dev/null; then
+    zypper refresh
+    zypper install -y git python3 python3-virtualenv python3-pip
+elif command -v pacman > /dev/null; then
+    pacman -Syu --noconfirm
+    pacman -S --noconfirm git python python-virtualenv python-pip
+elif command -v apk > /dev/null; then
+    apk update
+    apk add git python3 py3-virtualenv py3-pip
 else
     echo "Unsupported package manager. Please install git, python3, python3-venv, and python3-pip manually."
     exit 1
@@ -52,4 +55,5 @@ chmod +x main.py
 
 # Run the main.py script
 python3 main.py
+
 
